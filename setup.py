be98f894ad5724a73903183ce1192aace50487ae
helloracoon <useradd.temp@gmail.com>
2021-04-24 20:25:57 +0900

    init setup

--- setup.py
+++ setup.py
@@ -1,3 +1,15 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
-# TODO
+with open('requirements.txt') as f:
+	requirements = f.read().splitlines()
+	
+setup(
+	name="py-twitch",
+	version="0.0.1",
+	author="TEMP USER",
+	author_email="useradd.temp@gmail.com",
+	description="Twitch API Python client",
+	url="https://github.com/helloracoon/py-twitch",
+	packages=find_packages(),
+	install_requires=requirements
+)
