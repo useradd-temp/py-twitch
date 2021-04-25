b365d2682ee4be688d9c8ba57e3d246416670c36
helloracoon <useradd.temp@gmail.com>
2021-04-26 00:54:13 +0900

    bugfix

--- setup.py
+++ setup.py
@@ -3,13 +3,18 @@ from setuptools import setup, find_packages
 with open("requirements.txt") as f:
     requirements = f.read().splitlines()
 
+with open('README.md', encoding='utf-8') as f:
+    long_description = f.read()
+    
 setup(
     name="py-twitch",
-    version="0.0.1",
+    version="0.1.2",
     author="TEMP USER",
     author_email="useradd.temp@gmail.com",
     description="Twitch API Python client",
     url="https://github.com/helloracoon/py-twitch",
     packages=find_packages(),
     install_requires=requirements,
+    long_description=long_description,
+    long_description_content_type='text/markdown'
 )
