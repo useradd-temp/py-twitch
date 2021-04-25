c2fce79e33bb175433b9e0084d42912e678b09ea
helloracoon <useradd.temp@gmail.com>
2021-04-26 00:34:09 +0900

    empty

--- test.py
+++ test.py
@@ -1,13 +1,5 @@
 from twitch.client import TwitchAPIClient
 
 
-
-
-
-
-if __name__ == '__main__':
-	client_id = "YOUR CLIENT ID"
-	client_secret = "YOUR CLIENT SECRET"
-
-	client = TwitchAPIClient(client_id, client_secret)
-	print(client.get_users(login='twitch'))
+if __name__ == "__main__":
+    pass
